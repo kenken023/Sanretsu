18f0f2cc2bc79e2e053dd09c099000eb0ce2ae5d
Fritz <fritz@caresharing.ph>
2017-08-11 14:25:52 +0800

    Initial Project

---
+++ Sanretsu/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Sanretsu
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
