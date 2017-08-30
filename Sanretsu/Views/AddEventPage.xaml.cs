f9ad1566736f26088544c651db912f310d0d2fc2
Kenken <kenken_salar@yahoo.com>
2017-08-30 18:51:13 +0800

    Adding scanning page

--- Sanretsu/Views/AddEventPage.xaml.cs
+++ Sanretsu/Views/AddEventPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing;
 using ZXing.Net.Mobile.Forms;
 
 namespace Sanretsu.Views
@@ -15,16 +16,47 @@ namespace Sanretsu.Views
     {
         ZXingScannerView zxing;
         ZXingDefaultOverlay overlay;
+        private Grid _grid;
 
         public AddEventPage ()
 		{
 			InitializeComponent ();
+
+            //zxing = new ZXingScannerView
+            //{
+            //    HorizontalOptions = LayoutOptions.FillAndExpand,
+            //    VerticalOptions = LayoutOptions.FillAndExpand
+            //};
+
+            //overlay = new ZXingDefaultOverlay
+            //{
+            //    TopText = "Hold your phone up to the barcode",
+            //    BottomText = "Scanning will happen automatically",
+            //    ShowFlashButton = zxing.HasTorch,
+            //};
+            //zxing.IsScanning = false;
+
+            //zxing.OnScanResult += this.OnScanResult;
+
+
 
             zxing = new ZXingScannerView
             {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
                 VerticalOptions = LayoutOptions.FillAndExpand
             };
+            zxing.OnScanResult += (result) =>
+                Device.BeginInvokeOnMainThread(async () => {
+
+                    // Stop analysis until we navigate away so we don't keep reading barcodes
+                    zxing.IsAnalyzing = false;
+
+                    // Show an alert
+                    await DisplayAlert("Scanned Barcode", result.Text, "OK");
+
+                    // Navigate away
+                    await Navigation.PopAsync();
+                });
 
             overlay = new ZXingDefaultOverlay
             {
@@ -32,10 +64,9 @@ namespace Sanretsu.Views
                 BottomText = "Scanning will happen automatically",
                 ShowFlashButton = zxing.HasTorch,
             };
-        }
-
-        private async void OnScanClicked(object sender, EventArgs e)
-        {
+            overlay.FlashButtonClicked += (sender, e) => {
+                zxing.IsTorchOn = !zxing.IsTorchOn;
+            };
             var grid = new Grid
             {
                 VerticalOptions = LayoutOptions.FillAndExpand,
@@ -44,8 +75,60 @@ namespace Sanretsu.Views
             grid.Children.Add(zxing);
             grid.Children.Add(overlay);
 
-            StackContainer.Children.Add(grid);
+            // The root page of your application
+            Content = grid;
+        }
+
+        private void OnScanResult(Result result)
+        {
+            this.Code.Text = result.Text;
+            this.Type.Text = result.BarcodeFormat.ToString();
+        }
+
+        private void OnScanClicked(object sender, EventArgs e)
+        {
+            if (!zxing.IsScanning)
+            {
+                if (_grid == null)
+                {
+                    _grid = new Grid
+                    {
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                    };
+                    _grid.Children.Add(zxing);
+                    _grid.Children.Add(overlay);
+                    StackContainer.Children.Add(_grid);
+                }
+
+                StackContainer.Children.Add(_grid);
+                zxing.IsScanning = true;
+            }
+            else
+            {
+                StackContainer.Children.RemoveAt(StackContainer.Children.Count - 1);
+                zxing.IsScanning = false;
+            }
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
             zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
+
+        ~AddEventPage()
+        {
+            //zxing.OnScanResult -= this.OnScanResult;
         }
     }
 }
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -15,6 +15,7 @@ namespace Sanretsu.iOS
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
+            ZXing.Net.Mobile.Forms.iOS.Platform.Init();
 
 
             return base.FinishedLaunching(app, options);
