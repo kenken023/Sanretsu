a31d0790ebc0ba19c38ab3cb74d896c8e177f702
Fritz <fritz@caresharing.ph>
2017-08-31 15:23:20 +0800

    Scanning of attendee

--- Sanretsu/Views/AddEventPage.xaml.cs
+++ Sanretsu/Views/AddEventPage.xaml.cs
@@ -1,66 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using Sanretsu.Models;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-using ZXing;
-using ZXing.Mobile;
 using ZXing.Net.Mobile.Forms;
 
 namespace Sanretsu.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class AddEventPage : ContentPage
+    public partial class AddEventPage : ContentPage
     {
+        ZXingScannerView zxing;
+        ZXingDefaultOverlay overlay;
+        bool _isScanning = false;
 
         public AddEventPage()
-		{
+        {
             InitializeComponent();
+
+			zxing = new ZXingScannerView
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+			zxing.OnScanResult += (result) =>
+				Device.BeginInvokeOnMainThread(() => {
+
+					// Stop analysis until we navigate away so we don't keep reading barcodes
+					//zxing.IsAnalyzing = false;
+
+					// Show an alert
+					//await DisplayAlert("Scanned Barcode", result.Text, "OK");
+
+					//// Navigate away
+					//await Navigation.PopAsync();
+
+					this.Code.Text = result.Text;
+					this.Type.Text = result.BarcodeFormat.ToString();
+
+                    if (_isScanning)
+                    {
+						MessagingCenter.Send(this, "AddItem", new Item
+						{
+                            Id = result.Text,
+							Code = result.Text,
+							Text = result.Text,
+							Description = result.BarcodeFormat.ToString()
+						});
+                    }
+				});
+
+			overlay = new ZXingDefaultOverlay
+			{
+				TopText = "Hold your phone up to the barcode",
+				BottomText = "Scanning will happen automatically",
+				ShowFlashButton = zxing.HasTorch,
+			};
+			overlay.FlashButtonClicked += (sender, e) => {
+				zxing.IsTorchOn = !zxing.IsTorchOn;
+			};
+			var grid = new Grid
+			{
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+			};
+			grid.Children.Add(zxing);
+			grid.Children.Add(overlay);
+
+			// The root page of your application
+			//Content = grid;
+
+			this.StackContainer.Children.Add(grid);
         }
 
-        private async void OnScanClicked()
-        {
-            await this.InitScanner();
+        private void OnScanClicked(object sender, EventArgs e)
+		{
+            _isScanning = !_isScanning;
+
+            if (_isScanning)
+            {
+                this.BtnScan.Text = "Stop Scan";
+            }
+            else
+            {
+                this.BtnScan.Text = "Start Scan";
+            }
         }
 
-        private async Task InitScanner()
-        {
-            var options = new MobileBarcodeScanningOptions
-            {
-                AutoRotate = false,
-                TryInverted = true,
-                TryHarder = true
-            };
-            var scanPage = new ZXingScannerPage(options)
-            {
-                DefaultOverlayTopText = "Align the barcode within the frame",
-                DefaultOverlayBottomText = string.Empty,
-                DefaultOverlayShowFlashButton = true
-            };
-            // Navigate to our scanner page
-            
-            scanPage.OnScanResult += (result) => 
-            {
-                // Stop scanning
-                scanPage.IsScanning = false;
- 
-                // Pop the page and show the result
-                Device.BeginInvokeOnMainThread (async () => 
-                {
-                    await Navigation.PopAsync();        
-                    await DisplayAlert("Scanned Barcode", result.Text, "OK");
-                });
-            };
-            await Navigation.PushAsync(scanPage);
-        }
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
 
-        private void OnScanResult(Result result)
-        {
-            this.Code.Text = result.Text;
-            this.Type.Text = result.BarcodeFormat.ToString();
-        }
+			zxing.IsScanning = true;
+		}
+
+		protected override void OnDisappearing()
+		{
+			zxing.IsScanning = false;
+
+			base.OnDisappearing();
+		}
     }
 }
